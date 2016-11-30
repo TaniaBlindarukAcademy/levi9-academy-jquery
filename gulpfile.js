75d3df61fd51eafb2ca01fc283798ca3ce1a2e04
Tetiana Blindaruk <t.blindaruk@levi9.com>
2016-12-01 01:46:47 +0200

    Added bubel for project ;( Because *.js have template string and uglify can not merge it to *.min.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,11 @@ var requirejsOptimize = require('gulp-requirejs-optimize');
 var watch = require('gulp-watch');
 var batch = require('gulp-batch');
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var babel = require('gulp-babel');
 
 
-gulp.task('default', ['scripts','watch']);
+gulp.task('default', ['scripts','transpile','watch']);
 
 gulp.task('scripts', function () {
     return gulp.src('app.js')
@@ -20,6 +22,14 @@ gulp.task('scripts', function () {
         .pipe(gulp.dest('dist'));
 });
 
+gulp.task('transpile',function(){
+    return gulp.src('./dist/app.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('dist/es5'));
+});
+
 gulp.task('watch', function () {
     watch('js/*.js', batch(function (events, done) {
         gulp.start('scripts', done);
@@ -27,4 +37,10 @@ gulp.task('watch', function () {
     watch('app.js', batch(function (events, done) {
         gulp.start('scripts', done);
     }));
+    watch('dist/app.js', batch(function (events, done) {
+        gulp.start('transpile', done);
+    }));
 });
+
+
+
